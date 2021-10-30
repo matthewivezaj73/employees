faffe915e06e510779a31ad5d2e7cff82f38f784
Matthew <matthew.ivezaj73@gmail.com>
2021-10-30 10:32:03 -0400

    commit

--- employee_main.py
+++ employee_main.py
@@ -2,3 +2,6 @@
 from classes.check_employees import *
 #Setting a flag.
 not_employee = False
+#Testing for adding employees.
+while not not_employees:
+    decision = input("1. Would you like to add an employee.\n2. Show Current employees.\n3. Show seperated employees.\n4. Edit an employee.\n")
