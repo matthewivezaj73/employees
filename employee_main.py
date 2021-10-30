7980edf1e35c9f090c2526e3612289deb75382db
Matthew <matthew.ivezaj73@gmail.com>
2021-10-30 10:41:14 -0400

    Adding a case for if the user selects option 1.

--- employee_main.py
+++ employee_main.py
@@ -4,4 +4,7 @@ from classes.check_employees import *
 not_employee = False
 #Testing for adding employees.
 while not not_employee:
+    #Asking the user what they would like to do.
     decision = input("1. Create an employee.\n2. Show Current employees.\n3. Show seperated employees.\n4. Edit an employee.\n5. Import to database.\n6. Export a excel spreadsheet")
+    #Handing the case where the user selects option 1.
+    if decision.isdigit() == 1:
