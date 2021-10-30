52662cfa6300ba571ede8aa03efaf6fda2825ead
Matthew <matthew.ivezaj73@gmail.com>
2021-10-30 10:39:53 -0400

    Adding a number of decisions for the user to make.

--- employee_main.py
+++ employee_main.py
@@ -3,5 +3,5 @@ from classes.check_employees import *
 #Setting a flag.
 not_employee = False
 #Testing for adding employees.
-while not not_employees:
-    decision = input("1. Would you like to add an employee.\n2. Show Current employees.\n3. Show seperated employees.\n4. Edit an employee.\n")
+while not not_employee:
+    decision = input("1. Create an employee.\n2. Show Current employees.\n3. Show seperated employees.\n4. Edit an employee.\n5. Import to database.\n6. Export a excel spreadsheet")
