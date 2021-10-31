4e727c59aab30d2d2586adce6a6604db5e9a8359
Matthew <matthew.ivezaj73@gmail.com>
2021-10-31 09:35:22 -0400

    Added a comment.

--- employee_main.py
+++ employee_main.py
@@ -28,9 +28,11 @@ while not not_employee:
     if decision.isdigit() == 1:
         print("Starting the process of creating the employee...")
         not_first_name = False
+        #Testing for the employee first name.
         while not not_first_name:
             f_name = input("Please input your first name: ")
             not_first_name = my_employee.validateEmployee(f_name)
+        
         l_name = input("Please input your last name: ")
         combined_name = f"{f_name} {l_name}"
         dataOfHire = input("Please enter the employee date of hire: ")
